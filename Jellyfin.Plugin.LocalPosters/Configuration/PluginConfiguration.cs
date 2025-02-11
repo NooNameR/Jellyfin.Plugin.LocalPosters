b4856fff4f7be4722b2992b85e8eabb0521bcb2d
denys.kozhevnikov <github@mail.noonamer.com>
2025-02-11 09:52:54 +0000

    Remove breakpoint

--- Jellyfin.Plugin.LocalPosters/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.LocalPosters/Configuration/PluginConfiguration.cs
@@ -21,11 +21,21 @@ public class PluginConfiguration : BasePluginConfiguration
     {
         Folders =
         [
-            "/posters/christophedcgdriveposters", "/posters/reitenthgdriveposters", "/posters/mareaugdriveposters",
-            "/posters/dsaqgdriveposters", "/posters/quafleygdriveposters", "/posters/stupifierrgdriveposters",
-            "/posters/saharagdriveposters", "/posters/majorgiant1gdriveposters", "/posters/lioncitygamingdriveposters",
-            "/posters/iamspartacus1gdriveposters", "/posters/bzwartgdriveposters", "/posters/solengdriveposters",
-            "/posters/solen1gdriveposters", "/posters/zaroxgdriveposters", "/posters/drazzilbgdriveposters"
+            "/posters/christophedcgdriveposters",
+            "/posters/reitenthgdriveposters",
+            "/posters/mareaugdriveposters",
+            "/posters/dsaqgdriveposters",
+            "/posters/quafleygdriveposters",
+            "/posters/stupifierrgdriveposters",
+            "/posters/saharagdriveposters",
+            "/posters/majorgiant1gdriveposters",
+            "/posters/lioncitygamingdriveposters",
+            "/posters/iamspartacus1gdriveposters",
+            "/posters/bzwartgdriveposters",
+            "/posters/solengdriveposters",
+            "/posters/solen1gdriveposters",
+            "/posters/zaroxgdriveposters",
+            "/posters/drazzilbgdriveposters"
         ];
     }
 
